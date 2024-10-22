a8fea97097277ff158f552c41b919bf4d33bfc28
Taquispro <121485751+Taquispro@users.noreply.github.com>
2024-10-22 23:26:41 +0530

    Connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
+
+mongoose
+  .connect("mongodb://localhost:27017")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 app.listen(3000, () => {
   console.log("Server is running at port " + 3000);
