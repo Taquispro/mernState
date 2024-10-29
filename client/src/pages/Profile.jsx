6a50c24fbd5cf8408b559061164f83dee4fae5a7
Taquispro <121485751+Taquispro@users.noreply.github.com>
2024-10-30 02:43:21 +0530

    complete profile page ui

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,46 @@
+import { useSelector } from "react-redux";
 function Profile() {
-  return <div>Profile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  console.log(currentUser);
+  console.log(currentUser.avatar);
+  return (
+    <div className="p-3 max-w-screen-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7 ">Profile</h1>
+      <form className="flex flex-col">
+        <img
+          className="rounded-full h-24 w-24 object-covered cursor-pointer self-center mt-2 "
+          src={currentUser.avatar}
+          alt="Profile"
+        />
+
+        <input
+          type="text"
+          placeholder="username"
+          className="border p-3 rounded-lg"
+          id="username"
+        />
+        <input
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+          id="email"
+        />
+        <input
+          type="text"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+          id="password"
+        />
+        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
+          update
+        </button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span className="text-red-700 cursor-pointer">Sign out</span>
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
